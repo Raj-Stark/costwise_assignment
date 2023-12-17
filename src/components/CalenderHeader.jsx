6ae45c7603626062a4c3fd9742af55abe073cb2f
Raj Stark <rpal778866@gmail.com>
2023-12-17 11:25:31 +0530

    feat:Showing Current Month Inside Header Completed

--- src/components/CalenderHeader.jsx
+++ src/components/CalenderHeader.jsx
@@ -2,12 +2,32 @@ import React from "react";
 import { useGlobalContext } from "../context/CalenderContext";
 
 const CalenderHeader = () => {
-  const { changeMonth , dateObj } = useGlobalContext();
+  const { changeMonth, dateObj } = useGlobalContext();
+
+  const showMonthAndYear = () => {
+    const monthAndYear = `${dateObj.toLocaleDateString("en-US", {
+      month: "long",
+    })} ${dateObj.getFullYear()}`;
+
+    console.log(monthAndYear);
+
+    return monthAndYear;
+  };
   return (
     <div className=" bg-red-400 p-4 px-10 mt-16 h-10 flex justify-between items-center">
-      <button className=" px-6 bg-green-600  rounded" onClick={()=> changeMonth("DEC")}>Back</button>
-      <p>Current Month + Year</p>
-      <button className=" px-6 bg-green-600  rounded" onClick={()=> changeMonth("INC")}>Forward</button>
+      <button
+        className=" px-6 bg-green-600  rounded"
+        onClick={() => changeMonth("DEC")}
+      >
+        Back
+      </button>
+      <p>{showMonthAndYear()}</p>
+      <button
+        className=" px-6 bg-green-600  rounded"
+        onClick={() => changeMonth("INC")}
+      >
+        Forward
+      </button>
     </div>
   );
 };

f17d37832dcedf8a498c4770a37130a278536734
Raj Stark <rpal778866@gmail.com>
2023-12-18 20:20:51 +0530

    Fix : Slider issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="max-w-screen-sm xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md h-screen mx-auto  ">
       <CalenderPage></CalenderPage>
-      <div className="   absolute left-10 lg:left-10 top-0  flex justify-center items-center  h-full">
+      <div className=" hidden   absolute left-10 lg:left-10 top-0  lg:flex justify-center items-center  h-full">
           <CustomSlider></CustomSlider>
         </div>
     </div>
